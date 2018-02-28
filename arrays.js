0f93800c56dfbf0c7dba74f8f9674bf86225540e
Colin White <clwhite814@gmail.com>
2018-02-27 21:18:41 -0600

    Added index and arrays to the file

--- arrays.js
+++ arrays.js
@@ -10,3 +10,23 @@ if(test=== opp) {
 } else {
     console.log('same?', false); 
 }
+
+var animals = ['fish', 'dog', 'cat']; 
+var animalzElement = document.getElementById('animalz');
+
+for (var i=0; i<animals.length; i++){
+    console.log ("animals", animals[i])
+    animalzElement.innerHTML += '<h5>' + animals[i] + '<h5>'; 
+}
+
+//Challenge 2  
+
+var book = "catch 22"; 
+var bookSplit = book.split(""); 
+var realNumz = [];
+for(var j=0;j<bookSplit.length; j++){
+if((bookSplit[j]*1)){
+    realNumz.push(bookSplit[j]);
+}
+console.log("answer", realnumz.join("")) 
+
