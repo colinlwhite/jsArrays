cb5443f89efde88c4ca62c28c7b2ef3f6a584a92
Colin White <clwhite814@gmail.com>
2018-02-27 19:46:42 -0600

    We completed exercise 1

--- arrays.js
+++ arrays.js
@@ -1,1 +1,12 @@
-console.log
+console.log('hello'); 
+
+// Challenge 1 
+var test= "cat"; 
+
+var opp = test.split("").reverse().join("");
+
+if(test=== opp) {
+    console.log('same?', true);
+} else {
+    console.log('same?', false); 
+}
